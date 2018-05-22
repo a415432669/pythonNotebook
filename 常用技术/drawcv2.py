c403e7e7405dbf679be3e07a5340da1f04d9845e
Your Name <you@example.com>
2018-05-22 18:01:10 +0800

    '1234gjosajgoisag'

---
+++ 常用技术/drawcv2.py
@@ -0,0 +1,33 @@
+import numpy as np 
+import cv2
+import json
+
+from aip import AipFace
+import base64
+
+""" 你的 APPID AK SK """
+APP_ID = '11262633'
+API_KEY = 'vMCwrMzFb6AkgTqwac0IbXyg'
+SECRET_KEY = '2vIr1XO9wviq0ma5KFKt0huC8fbhhwYo'
+imageType = "BASE64"
+
+client = AipFace(APP_ID, API_KEY, SECRET_KEY)
+jpg1 = base64.b64encode(open('./3.jpg', 'rb').read())
+jpg1 = bytes.decode(jpg1)
+res =  client.detect(jpg1, imageType)
+# res =  json.loads(result)
+
+jpg1 =  cv2.imread('./3.jpg')
+for item in res['result']['face_list']:
+    x1 = int(item['location']['left'])
+    y1 = int(item['location']['top'])
+    x2 = int(item['location']['left']+item['location']['width'])
+    y2 = int(item['location']['top']+item['location']['height'])
+    cv2.rectangle(jpg1, (x1,y1), (x2,y2), (0, 255, 0), 2)
+
+
+
+
+
+cv2.imshow('capture',jpg1)
+cv2.waitKey(0)
---
+++ 常用技术/opencv.py
@@ -0,0 +1,27 @@
+﻿import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+# while(1):
+#     # get a frame
+#     ret, frame = cap.read()
+#     # show a frame
+#     cv2.imshow("capture", frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+try :
+    
+    # show a frame
+    n = 0
+    while(n<10):
+        n = n + 1
+        ret, frame = cap.read()
+        cv2.imshow("capture", frame)
+        cv2.imwrite('./3.jpg',frame)
+except Exception as e:
+    print(e)
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
